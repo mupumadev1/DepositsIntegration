c618ede40dddca2777c016880f68224c52bf5ebd
Serita.Kunda <seriterkunda@mupuma.co.zm>
2023-06-13 02:54:51 -0700

    Sage Infratel Payables Web Service

--- DepositsIntegrationService/settings.py
+++ DepositsIntegrationService/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'webapp.apps.WebappConfig',
     'widget_tweaks',
     'django_htmx',
-    'corsheaders',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
@@ -138,7 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
